b67120f9710ab1e3e52b3084ef56163d2e5c66af
Ryan <ryandam1473@googlemail.com>
2026-03-02 00:18:02 +0000

    setup basic web server

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func setupRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintln(w, "Hello!")
+		if err != nil {
+			return
+		}
+	})
+
+	return mux
+}
+
+func main() {
+	mux := setupRoutes()
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
